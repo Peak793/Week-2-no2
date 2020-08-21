0abb147203d1b746fb247f4439231013b50c6279
CHANWANIT NUCHYOO <63010224@KMITL.AC.TH>
2020-08-21 14:55:32 +0700

    Final

--- Week 2 no2/Source.cpp
+++ Week 2 no2/Source.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-void bubbleSort(int*p,int n)
+void bubbleSort(int* p,int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
@@ -17,19 +17,49 @@ void bubbleSort(int*p,int n)
 		}
 	}
 }
+int gcd(int* p, int n)
+{
+	int i, j, a = 0, gcd = 1;
+	for (i = 2; i <=*(p+0); i++)
+	{
+		a = 0;
+		for (j = 0; j < n; j++)
+		{
+			if (*(p + j) % i == 0)
+			{
+				a+=1;
+				if (a == n)
+				{
+					gcd = i;
+				}
+			}
+		}
+	}
+	return gcd;
+}
 int main()
 {
 	int* p;
-	int n,i;
-	int x[100];
+	int n, i;
 	scanf("%d", &n);
+	if (n < 2 || n>5)
+	{
+		printf("Error");
+		return 0;
+	}
 	p = (int*)malloc(n * sizeof(int));
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", (p + i));
+		if (*(p + i) <= 1 || *(p + i) > 10000)
+		{
+			printf("Error");
+			return 0;
+		}
 	}
 	bubbleSort(p, n);
 	int r = 0;
+	int gcd1 = gcd(p, n);
 	neww :
 	printf("%d : ",*(p+r));
 	repeat :
@@ -56,4 +86,5 @@ int main()
 			goto  neww;
 		}
 	}
+	printf("\nGCD : %d\n", gcd1);
 }
